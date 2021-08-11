248bfb59a1821082904fcce9bcae446bfa7afd46
CyberGA <etomu.joshua@gmail.com>
2021-08-11 22:50:29 +0100

    fixed multiDex error and firebase errors

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tungar Messenger',
       theme: ThemeData(
         primarySwatch: Colors.red,
         textTheme: TextTheme(
