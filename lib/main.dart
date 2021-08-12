f055d728d2e2687b1e52cd81b05f1d3a0dc43cac
ikxyz <40415727+Ikxyz@users.noreply.github.com>
2021-08-12 17:04:47 +0100

    update git ignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:super_todo/pages/SplashScreen.dart';
 import 'package:super_todo/pages/chat.dart';
 import 'package:super_todo/pages/onboarding/onboarding.dart';
 import 'package:super_todo/styles/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'pages/Login.dart';
 import 'pages/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -21,21 +25,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
         textTheme: TextTheme(
-          button: GoogleFonts.lato(color: cLight,fontWeight: FontWeight.bold,fontSize: 18),
-          headline1: GoogleFonts.lato(color: cLight,fontWeight: FontWeight.bold),
-          headline3: GoogleFonts.lato(color: cLight,fontWeight: FontWeight.bold),
-          headline5: GoogleFonts.lato(color: cLight)
-          ),
+            button: GoogleFonts.lato(
+                color: cLight, fontWeight: FontWeight.bold, fontSize: 18),
+            headline1:
+                GoogleFonts.lato(color: cLight, fontWeight: FontWeight.bold),
+            headline3:
+                GoogleFonts.lato(color: cLight, fontWeight: FontWeight.bold),
+            headline5: GoogleFonts.lato(color: cLight)),
       ),
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       initialRoute: SplashScreen.route,
       routes: {
-        SplashScreen.route:(BuildContext context)=>SplashScreen(),
-        OnBoarding.route:(BuildContext context)=>OnBoarding(),
+        SplashScreen.route: (BuildContext context) => SplashScreen(),
+        OnBoarding.route: (BuildContext context) => OnBoarding(),
         Login.route: (BuildContext context) => Login(),
         Home.route: (BuildContext context) => Home(),
-          Chat.route: (BuildContext context) => Chat(),
+        Chat.route: (BuildContext context) => Chat(),
       },
     );
   }
--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,4 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:super_todo/firebase.dart';
+import 'package:super_todo/module/crypto.dart';
 import 'package:super_todo/pages/home.dart';
 import 'package:super_todo/styles/colors.dart';
 
@@ -9,8 +14,62 @@ class Login extends StatelessWidget {
 
   Login({Key? key}) : super(key: key);
 
-  void login() {
+  void loginAsGuest() async {
+    final user = await signInAsGuest();
+
+    if (user == null) return;
+
     Navigator.of(context).pushNamedAndRemoveUntil(Home.route, (route) => false);
+  }
+
+  void loginWithGoogle() async {
+    final user = (await signInWithGoogle()) ?? (await signInAsGuest());
+
+    if (user == null) return;
+
+    Navigator.of(context).pushNamedAndRemoveUntil(Home.route, (route) => false);
+  }
+
+  Future<UserCredential?> signInAsGuest() async {
+    final guestAccount = await fAuth.signInAnonymously();
+    final user = guestAccount.user;
+
+    if (user == null) return null;
+
+    // update email
+    final email = '${user.uid}@tungar.com';
+
+    final photoUrl =
+        "https://www.gravatar.com/avatar/${Crypto.hash(email, CryptoAlg.md5)}?d=identicon&s=250";
+
+    await user.updateEmail(email);
+
+    await user.updatePhotoURL(photoUrl);
+
+    
+
+    // update profile photo
+
+    return guestAccount;
+  }
+
+  /// Sign In With Google Auth
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      final googleSignIn = await GoogleSignIn().signIn();
+
+      if (googleSignIn == null) return null;
+
+      final googleAuth = await googleSignIn.authentication;
+
+      final googleAuthProviderCredential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+
+      return fAuth.signInWithCredential(googleAuthProviderCredential);
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 
   @override
@@ -52,7 +111,7 @@ class Login extends StatelessWidget {
                 ),
                 Center(
                   child: OutlinedButton.icon(
-                      onPressed: login,
+                      onPressed: loginAsGuest,
                       style: ButtonStyle(
                           shape: MaterialStateProperty.all(StadiumBorder()),
                           elevation: MaterialStateProperty.all(0),
@@ -70,7 +129,7 @@ class Login extends StatelessWidget {
                 ),
                 Center(
                   child: ElevatedButton.icon(
-                      onPressed: login,
+                      onPressed: loginWithGoogle,
                       style: ButtonStyle(
                           shape: MaterialStateProperty.all(StadiumBorder()),
                           elevation: MaterialStateProperty.all(0),
@@ -92,4 +151,3 @@ class Login extends StatelessWidget {
     );
   }
 }
-
