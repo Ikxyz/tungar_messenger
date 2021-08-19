911a7eab557da929327a99330dae3b238fb2552b
CyberGA <etomu.joshua@gmail.com>
2021-08-19 15:31:15 +0100

    fixed bug on AndroidManifest

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:super_todo/pages/SplashScreen.dart';
 import 'package:super_todo/pages/chat.dart';
